82b544cdaa491d1d587cd6372cf8fdc1737eef54
alexp <alexpelnar@gmail.com>
2017-10-10 17:02:55 -0500

    Whole Project

---
+++ PostIt_Master/PostIt_Master/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PostIt_Master.Startup))]
+namespace PostIt_Master
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
