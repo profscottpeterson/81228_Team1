5a8b89781331aca2b1fd6398d5e3ab9def3f31f8
rikis <rikis@DESKTOP-LV5LV59>
2017-10-29 19:16:57 -0500

    Update to create/delete notes.  Alex edits

---
+++ PostIt_Master/PostIt_Master/Models/CreateNote.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PostIt_Master.Models;
+
+namespace PostIt_Master.Models
+{
+    public class CreateNote
+    {
+        public static Note findNote(IEnumerable<Note> passedNote)
+        {
+            NWTCTeamDevDatabaseEntities db = new NWTCTeamDevDatabaseEntities();
+            var stickies = db.Notes.ToList();
+            Note result = new Note();
+            if(passedNote != null)
+            {
+                foreach (var stick in stickies)
+                {
+                    foreach (var note in passedNote)
+                    {
+                        if (note.NoteId == stick.NoteId) result = note;
+                    }
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
